ea82c3178414818ba9286b8d44d019c5800ba884
casjr13 <casjr13@users.noreply.github.com>
2020-01-02 07:54:04 +0000

    Automated deployment: Thu Jan  2 07:54:04 UTC 2020 a2a372370898a7726644b02e8b36fd40a5a7ecd6

---
+++ precache-manifest.d1960fa3bd9bf58e98b5bf2a52677b75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e9470f789101b7cebc230a2c2e8ccd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48923ccd01177bbbcd11",
+    "url": "/static/css/main.ad2b70d2.chunk.css"
+  },
+  {
+    "revision": "027c0c592a6d8203ec04",
+    "url": "/static/js/2.f2c0514d.chunk.js"
+  },
+  {
+    "revision": "2fd49168760421d6eedb038b586f062d",
+    "url": "/static/js/2.f2c0514d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48923ccd01177bbbcd11",
+    "url": "/static/js/main.bf32b6c2.chunk.js"
+  },
+  {
+    "revision": "9018146f8fde92165a14",
+    "url": "/static/js/runtime-main.839bb005.js"
+  },
+  {
+    "revision": "a72f959694dbd6c76058eb444267cef4",
+    "url": "/static/media/amber.a72f9596.jpg"
+  },
+  {
+    "revision": "70668951e3d29b7d7dbf12a4af255902",
+    "url": "/static/media/me.70668951.jpg"
+  }
+]);
