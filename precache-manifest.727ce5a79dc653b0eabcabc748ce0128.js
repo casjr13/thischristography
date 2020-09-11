9a53f55083d31cad2f2a9612d7fafce3f7767343
casjr13 <casjr13@users.noreply.github.com>
2020-09-11 02:04:46 +0000

    Automated deployment: Fri Sep 11 02:04:46 UTC 2020 e6035cdb136d2b618adf23185bb0ae23c940eda4

---
+++ precache-manifest.727ce5a79dc653b0eabcabc748ce0128.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a964ac19cce38755335144fc36ee2579",
+    "url": "/index.html"
+  },
+  {
+    "revision": "044a3d4a71723e42dfd6",
+    "url": "/static/css/main.cb4580fa.chunk.css"
+  },
+  {
+    "revision": "0097ab263b60c35780a6",
+    "url": "/static/js/2.93262450.chunk.js"
+  },
+  {
+    "revision": "45a2c04b17b5fa46b6c4dc216cdeeda5",
+    "url": "/static/js/2.93262450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "044a3d4a71723e42dfd6",
+    "url": "/static/js/main.9a1d64dd.chunk.js"
+  },
+  {
+    "revision": "12acc65cf52aff6850fa",
+    "url": "/static/js/runtime-main.708e7fb4.js"
+  },
+  {
+    "revision": "a72f959694dbd6c76058eb444267cef4",
+    "url": "/static/media/amber.a72f9596.jpg"
+  },
+  {
+    "revision": "70668951e3d29b7d7dbf12a4af255902",
+    "url": "/static/media/me.70668951.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.db4689813ac3f2097c2b7a8840f59c35.js"
+  "/precache-manifest.727ce5a79dc653b0eabcabc748ce0128.js"
 );
 
 self.addEventListener('message', (event) => {
